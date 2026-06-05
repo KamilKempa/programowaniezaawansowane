c1c240d21af335fc0bff2ecc4aa097dc48fecf69
Kamil <chx102418@student.chorzow.merito.pl>
2026-06-05 12:59:12 +0200

    dodanie route

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,24 @@ app = FastAPI()
 @app.get("/")
 def root():
     return{"route works"}
+
+@app.get("/testowa")
+def testowa():
+    return{"testowa"}
+
+# parametr path http://localhost:8000/currency/PLN
+@app.get("/currency/{code}")
+def currencies(code: str):
+    return{"currency": code}
+
+# parametr query http://localhost:8000/rates?date=13-03-2028
+@app.get("/rates")
+def get_rates(date: str):
+    return {"date": date}
+
+@app.post("/currencies/fetch")
+def fetch_currencies():
+    return {
+        "status": "success",
+        "message": "pobieranie kursow walut"
+    }
